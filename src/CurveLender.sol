e6f2c828153c9636eecc97bd832ad1429ceabef2
dudesahn <dudesahn@pm.me>
2024-04-29 21:51:48 -0400

    fix: no need for min

--- src/CurveLender.sol
+++ src/CurveLender.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.18;
 
-import {Base4626Compounder, ERC20, SafeERC20, Math} from "@periphery/Bases/4626Compounder/Base4626Compounder.sol";
+import {Base4626Compounder, ERC20, SafeERC20} from "@periphery/Bases/4626Compounder/Base4626Compounder.sol";
 import {TradeFactorySwapper} from "@periphery/swappers/TradeFactorySwapper.sol";
 import {IStaking} from "./interfaces/CurveInterfaces.sol";
 
@@ -50,11 +50,7 @@ contract CurveLender is Base4626Compounder, TradeFactorySwapper {
     }
 
     function vaultsMaxWithdraw() public view override returns (uint256) {
-        return
-            Math.min(
-                vault.convertToAssets(vault.maxRedeem(address(staking))),
-                vault.totalAssets()
-            );
+        return vault.convertToAssets(vault.maxRedeem(address(staking)));
     }
 
     /* ========== TRADE FACTORY FUNCTIONS ========== */
